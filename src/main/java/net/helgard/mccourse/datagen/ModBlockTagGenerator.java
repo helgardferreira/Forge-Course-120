6c75f988e7d0872190d95d2e17e598219945a7a5
Helgard R. Ferreira <hrfer3@gmail.com>
2025-01-16 02:27:11 +0200

    feat: update tag data generation

--- src/main/java/net/helgard/mccourse/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/helgard/mccourse/datagen/ModBlockTagGenerator.java
@@ -5,6 +5,7 @@ import net.helgard.mccourse.block.ModBlocks;
 import net.helgard.mccourse.util.ModTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -12,6 +13,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
+
+import static net.minecraftforge.common.Tags.Blocks.*;
 
 public class ModBlockTagGenerator extends BlockTagsProvider {
     public ModBlockTagGenerator(
@@ -26,7 +29,42 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
     protected void addTags(HolderLookup.@NotNull Provider lookupProvider) {
         this.tag(ModTags.Blocks.METAL_DETECTOR_VALUABLES)
                 .add(ModBlocks.ALEXANDRITE_ORE.get())
-                .addTag(Tags.Blocks.ORES);
+                .add(ModBlocks.DEEPSLATE_ALEXANDRITE_ORE.get())
+                .add(ModBlocks.END_STONE_ALEXANDRITE_ORE.get())
+                .add(ModBlocks.NETHER_ALEXANDRITE_ORE.get())
+                .add(ModBlocks.RAW_ALEXANDRITE_BLOCK.get())
+                .addTag(Tags.Blocks.ORES)
+                .addTag(Tags.Blocks.ORES_NETHERITE_SCRAP)
+                .addTag(Tags.Blocks.STORAGE_BLOCKS_COAL)
+                .addTag(Tags.Blocks.STORAGE_BLOCKS_EMERALD)
+                .addTag(Tags.Blocks.STORAGE_BLOCKS_LAPIS)
+                .addTag(Tags.Blocks.STORAGE_BLOCKS_NETHERITE)
+                .addTag(Tags.Blocks.STORAGE_BLOCKS_QUARTZ)
+                .addTag(Tags.Blocks.STORAGE_BLOCKS_RAW_COPPER)
+                .addTag(Tags.Blocks.STORAGE_BLOCKS_RAW_GOLD)
+                .addTag(Tags.Blocks.STORAGE_BLOCKS_RAW_IRON)
+                .addTag(Tags.Blocks.STORAGE_BLOCKS_REDSTONE);
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.ALEXANDRITE_BLOCK.get())
+                .add(ModBlocks.ALEXANDRITE_ORE.get())
+                .add(ModBlocks.DEEPSLATE_ALEXANDRITE_ORE.get())
+                .add(ModBlocks.END_STONE_ALEXANDRITE_ORE.get())
+                .add(ModBlocks.NETHER_ALEXANDRITE_ORE.get())
+                .add(ModBlocks.RAW_ALEXANDRITE_BLOCK.get())
+                .add(ModBlocks.SOUND_BLOCK.get())
+        ;
+
+        this.tag(BlockTags.MINEABLE_WITH_AXE)
+                .add(ModBlocks.SOUND_BLOCK.get());
+
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.ALEXANDRITE_BLOCK.get())
+                .add(ModBlocks.ALEXANDRITE_ORE.get())
+                .add(ModBlocks.DEEPSLATE_ALEXANDRITE_ORE.get())
+                .add(ModBlocks.END_STONE_ALEXANDRITE_ORE.get())
+                .add(ModBlocks.NETHER_ALEXANDRITE_ORE.get())
+                .add(ModBlocks.RAW_ALEXANDRITE_BLOCK.get());
     }
 
     @Override
