0e80b3d5b6d15f72aa27172c49ce7c861b0cb3bc
Helgard R. Ferreira <hrfer3@gmail.com>
2025-01-16 02:01:22 +0200

    feat: update ore drop count

--- src/main/java/net/helgard/mccourse/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/helgard/mccourse/datagen/loot/ModBlockLootTables.java
@@ -4,7 +4,14 @@ import net.helgard.mccourse.block.ModBlocks;
 import net.helgard.mccourse.item.ModItems;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
 
@@ -23,30 +30,38 @@ public class ModBlockLootTables extends BlockLootSubProvider {
 
         this.add(
                 ModBlocks.ALEXANDRITE_ORE.get(),
-                block -> createOreDrop(
+                block -> createOreDropWithCount(
                         ModBlocks.ALEXANDRITE_ORE.get(),
-                        ModItems.RAW_ALEXANDRITE.get()
+                        ModItems.RAW_ALEXANDRITE.get(),
+                        1.0f,
+                        1.0f
                 )
         );
         this.add(
                 ModBlocks.DEEPSLATE_ALEXANDRITE_ORE.get(),
-                block -> createOreDrop(
+                block -> createOreDropWithCount(
                         ModBlocks.DEEPSLATE_ALEXANDRITE_ORE.get(),
-                        ModItems.RAW_ALEXANDRITE.get()
+                        ModItems.RAW_ALEXANDRITE.get(),
+                        1.0f,
+                        2.0f
                 )
         );
         this.add(
                 ModBlocks.END_STONE_ALEXANDRITE_ORE.get(),
-                block -> createOreDrop(
+                block -> createOreDropWithCount(
                         ModBlocks.END_STONE_ALEXANDRITE_ORE.get(),
-                        ModItems.RAW_ALEXANDRITE.get()
+                        ModItems.RAW_ALEXANDRITE.get(),
+                        2.0f,
+                        4.0f
                 )
         );
         this.add(
                 ModBlocks.NETHER_ALEXANDRITE_ORE.get(),
-                block -> createOreDrop(
+                block -> createOreDropWithCount(
                         ModBlocks.NETHER_ALEXANDRITE_ORE.get(),
-                        ModItems.RAW_ALEXANDRITE.get()
+                        ModItems.RAW_ALEXANDRITE.get(),
+                        2.0f,
+                        4.0f
                 )
         );
     }
@@ -57,5 +72,17 @@ public class ModBlockLootTables extends BlockLootSubProvider {
                 .getEntries()
                 .stream()
                 .map(RegistryObject::get)::iterator;
+    }
+
+    private LootTable.Builder createOreDropWithCount(Block block, Item item, float min, float max) {
+        return createSilkTouchDispatchTable(
+                block,
+                this.applyExplosionDecay(
+                        block,
+                        LootItem.lootTableItem(item)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(min, max)))
+                                .apply(ApplyBonusCount.addOreBonusCount(Enchantments.FORTUNE))
+                )
+        );
     }
 }
